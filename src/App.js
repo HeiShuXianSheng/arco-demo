e726f305cae759840f7ffe4357f2b143cfdc44f0
HeiShuXianSheng <wf.wangfei@foxmail.com>
2022-09-14 18:28:30 +0800

    bug：Pagination切换 pagesize 时页面会闪现滚动条

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import { Pagination } from "@arco-design/web-react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* empty 的作用是将 Pagination 组件撑到页面的下方。 */}
+      <div className='empty'></div>
+
+      {/* 发现 Pagination 组件位于页面下半部分时，页面上改变每页展示条目(PageSize)时会闪现页面右侧滚动条 */}
+      <Pagination total={50} showTotal showJumper sizeCanChange />
     </div>
   );
 }
